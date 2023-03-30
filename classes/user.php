71624117adde3a8608a25b5a9e3930f3ebe77592
fbruggeling <40127266+fbruggeling@users.noreply.github.com>
2023-03-30 20:06:31 +0200

    first commit

---
+++ classes/user.php
@@ -0,0 +1,142 @@
+<?php
+    // Functie: classdefinitie User 
+    // Auteur: Wigmans
+
+    class User{
+
+        // Eigenschappen 
+        public $username;
+        public $email;
+        private $password;
+        
+        function SetPassword($password){
+            $this->password = $password;
+        }
+        function GetPassword(){
+            return $this->password;
+        }
+
+        public function ShowUser() {
+            echo "<br>Username: j<br>";
+            echo "<br>Password: $this->password<br>";
+            echo "<br>Email: $this->email<br>";
+        }
+
+        public function RegisterUser(){
+            $status = false;
+            $errors=[];
+            if($this->username != "" || $this->password != ""){
+
+                // Check user exist
+                if(true){
+                    array_push($errors, "Username bestaat al.");
+                } else {
+                    // username opslaan in tabel login
+                    // INSERT INTO `user` (`username`, `password`, `role`) VALUES ('kjhasdasdkjhsak', 'asdasdasdasdas', '');
+                    // Manier 1
+                    
+                    $status = true;
+                }
+                            
+                
+            }
+            return $errors;
+        }
+
+        function ValidateUser(){
+            $errors=[];
+
+            if (empty($this->username)){
+                array_push($errors, "Invalid username");
+            } else if (empty($this->password)){
+                array_push($errors, "Invalid password");
+            }
+
+            // Test username > 3 tekens
+
+            if(strlen($this->username) < 3){
+                array_push($errors, "username te kort");
+            }
+            
+            return $errors;
+        }
+
+        public function LoginUser(){
+
+            // Connect database
+            try {
+                $pdo = new PDO("mysql:host=localhost;dbname=loginsysteem", 'root', '');
+                // Set PDOException
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo 'Connected Succesfully';
+            } catch(PDOException $e) {
+                echo "Connection failed: " . $e->getMessage();
+            }
+
+            // Zoek user in de table user
+
+            $query = "SELECT * FROM user WHERE username = :username AND password = :password";
+            $stmt = $pdo->prepare($query);
+            $stmt->execute(
+                array(
+                    "username"=> $this->username,
+                    "password" => $this->password
+                )
+            );
+
+            $count = $stmt->rowCount();
+
+            // Indien gevonden dan sessie vullen
+
+            if($count > 0) {
+                session_start();
+
+                $_SESSION["username"] = $this->username;
+
+                return true;
+            } else {
+                echo 'Verkeerde input';
+                return false;
+            }
+
+        }
+
+        // Check if the user is already logged in
+        public function IsLoggedin() {
+            // Check if user session has been set
+            if(isset($_SESSION["username"])) {
+                return true;
+            } else {
+                return false;
+            }
+            
+        }
+
+        public function GetUser($username){
+
+		    // Doe SELECT * from user WHERE username = $username
+            if (false){
+                //Indien gevonden eigenschappen vullen met waarden uit de SELECT
+                $this->username = $_SESSION['username'];
+            } else {
+                return NULL;
+            }   
+        }
+
+        public function Logout(){
+            session_start();
+            // remove all session variables
+            unset($_SESSION['username']);
+
+            // destroy the session
+            session_destroy();
+
+            header('location: index.php');
+        }
+
+
+    }
+
+    
+
+?>
---
+++ loginsysteem.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 30, 2023 at 07:50 PM
+-- Server version: 8.0.28
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loginsysteem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `username` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `email`) VALUES
+(1, 'fedor', 'test123', 'test@test.nl');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
